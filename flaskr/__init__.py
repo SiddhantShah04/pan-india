76b6f9250535abea81a4c820126676d7a6772f56
Siddhant Shah <siddhantshah04@gmail.com>
2021-08-21 09:59:39 +0530

    fixes

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -20,38 +20,14 @@ def create_app(test_config=None):
     # app.config['SERVER_NAME'] = "xyz.localhost:5000"
     app.config['SESSION_PERMANENT'] = True
     app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=1400)
-    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-
-    # app.config['MAIL_SERVER'] = environ.get("MAIL_SERVER")
-    # app.config['MAIL_PORT'] = environ.get("MAIL_PORT")
-    # app.config['MAIL_USERNAME'] = environ.get("MAIL_USERNAME")
-    # app.config['MAIL_PASSWORD'] = environ.get("MAIL_PASSWORD")
-    # app.config['MAIL_USE_TLS'] = environ.get("MAIL_USE_TLS")
-    # app.config['MAIL_USE_SSL'] = environ.get("MAIL_USE_SSL")
-
-    # @app.before_request
-    # def before_request():
-    #     client = request.host.split('.')
-    #     app.config['SERVER_NAME'] = f"{client[0]}.localhost:5000"
+    # cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     api = Api(app, version='1.0', title='Sample API',
               description='A sample API')
 
-    from . import auth
-    api.add_namespace(auth.api)
+    @api.route('/')
+    class HelloWorld(Resource):
+        def get(self):
+            return("ghj")
 
-    from . import client
-    api.add_namespace(client.api)
-
-    from . import image
-    api.add_namespace(image.api)
-
-    from . import users
-    api.add_namespace(users.api)
-
-    from . import order
-    api.add_namespace(order.api)
-
-    from . import product
-    api.add_namespace(product.api)
     return app
